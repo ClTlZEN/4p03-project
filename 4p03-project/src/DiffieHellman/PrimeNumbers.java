a3201eef4a009cdb5f3732e14325ad6481f27979
ClTlZEN <spoof_2823@hotmail.com>
2015-04-19 16:19:34 -0400

    Computed q

--- 4p03-project/src/DiffieHellman/PrimeNumbers.java
+++ 4p03-project/src/DiffieHellman/PrimeNumbers.java
@@ -1,24 +1,56 @@
 package DiffieHellman;
 
 import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.BitSet;
 import java.util.Random;
 
 public class PrimeNumbers{
 	
 	private BigInteger p;
 	private BigInteger q;
-	private int l;
+	private double l;
+	private BigInteger hash1;
+	private BigInteger hash2;
 	
-	public PrimeNumbers(int el){
+	public PrimeNumbers(double el){
 		l = el;
 	}
-	public void nistGetPrimes(int l){
-		int L = 512 + 65*(l);
-		
+	
+	//Using l=2, b=100
+	public void nistGetPrimes() throws NoSuchAlgorithmException{
+		double L = 512 + 65*(l);
+		double b = 100;
+		double n = (L - 1 - b)/160;
 		boolean probPrime = false;
 		
-		while (!probPrime){
+		//Should be while
+		if (!probPrime){
+			byte[] seed1 = randSeed();
+			hash1 = new SHA1Hash(seed1).getHash();
+			
+			//s
+			BigInteger seed1Int = new BigInteger(seed1);
+			
+			//(s+1) mod 2^160
+			BigInteger seed2Int = (seed1Int.add(BigInteger.ONE).mod(BigInteger.valueOf((long) Math.pow(2,160))));
+			byte[] seed2 = seed2Int.toByteArray();
+			hash2 = new SHA1Hash(seed2).getHash();
+			
+			//Setting most significant bit to 1, AKA, making sure it's negative
+			BigInteger U = hash1.xor(hash2);
+			if (U.compareTo(BigInteger.ZERO)>0){
+				U = U.negate();
+			}
+			//Setting least significant bit to 1, AKA, making sure it's odd
+			if(U.mod(new BigInteger("2")).equals(BigInteger.ZERO)){
+				U = U.add(BigInteger.ONE);
+			}
+			
+			//Now that you have modified U, set it as q
+			BigInteger q = U;
+			
 			
 		}
 		
@@ -27,12 +59,11 @@ public class PrimeNumbers{
 		return false;
 		
 	}
+	
 	public byte[] randSeed(){
 		
 		Random rand = new SecureRandom();
-		
 		byte[] randSeed = new byte[20];
-		
 		rand.nextBytes(randSeed);
 		
 		return randSeed;
