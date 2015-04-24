1ad2716bd12abd93741c0ce07c974dc9b1f2e4b6
ClTlZEN <spoof_2823@hotmail.com>
2015-04-24 00:17:03 -0400

    Added networking ability. Needs refinement for key exchange.

---
+++ 4p03-project/src/DiffieHellman/CommServer.java
@@ -0,0 +1,26 @@
+package DiffieHellman;
+ 
+import java.net.*;
+import java.io.*;
+ 
+public class CommServer {
+    public static void main(String[] args) throws IOException {
+ 
+	    /*if (args.length != 1) {
+	        System.err.println("Usage: java KKMultiServer <port number>");
+	        System.exit(1);
+	    }*/
+ 
+        int portNumber = 4000;
+        boolean listening = true;
+         
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) { 
+            while (listening) {
+                new CommThread(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}
