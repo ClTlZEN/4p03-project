f742c524ec3fd99786f008cf6a9559531c91467d
Annzubork <annuschka.bork@gmail.com>
2015-04-24 11:21:02 -0400

    Added circle class and accessor methods to directly calculate the keys w/out networking.

--- 4p03-project/src/DiffieHellman/KeyExchange.java
+++ 4p03-project/src/DiffieHellman/KeyExchange.java
@@ -8,8 +8,21 @@ public class KeyExchange {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException, FileNotFoundException, UnsupportedEncodingException {
 		// TODO Auto-generated method stub
-		PrimeNumbers primes = new PrimeNumbers(4);
-		primes.nistGetPrimes();
+		//PrimeNumbers primes = new PrimeNumbers(4);
+		//primes.nistGetPrimes();
+		/*Communicator Alice = new Communicator(2, 1);
+		Communicator Bob = new Communicator(2,2);
+		
+		Alice.getKey(Bob.sendKey());
+		System.out.println("Alice's received keys: ");
+		Alice.printKeys();
+		Bob.getKey(Alice.sendKey());
+		System.out.println("Bob's received keys: ");
+		Bob.printKeys();*/
+		
+		Circle peeps = new Circle(4);
+		
+		
 	}
 
 }
